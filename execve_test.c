b4d75543719b1dcd8bf17323c278fccd71390bb0
joel <joelsantossouza08@gmail.com>
2025-11-26 08:47:08 +0000

    Understanding pipex a lot better

--- execve_test.c
+++ execve_test.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include "libft/libft.h"
 #include <stdlib.h>
+#include <sys/wait.h>
 
 int	listlen(char **argv)
 {
@@ -15,13 +16,21 @@ int	listlen(char **argv)
 int	main(int argc, char **argv, char **envp)
 {
 	int		pid;
+	int		pid2;
 	char	**splited;
+	int		fds[2];
 
 	if (argc != 5)
 		return (1);
+	if (pipe(fds) < 0)
+		return (2);
 	pid = fork();
+
 	if (pid == 0)
 	{
+		dup2(fds[1], 1);
+		close(fds[0]);
+		close(fds[1]);
 		splited = ft_split(argv[2], ' ');
 		if (!splited)
 			return (2);
@@ -35,16 +44,21 @@ int	main(int argc, char **argv, char **envp)
 		ft_fprintf(2, "Execve failed\n");
 		free(splited); // GIVES LEAK ONCE THE PROGRAM SWAP THE CURRENT CODE AND MEMORY SEGMENT OF THE PROCESS
 	}
-	else
+	else if (0 == (pid2 = fork()))
 	{
-		char	result[1000];
-		int bytes_read = read(1, result, 1000);
-		write(0, result, bytes_read);
-		splited = ft_split(argv[2], ' ');
+		dup2(fds[0], 0);
+		close(fds[0]);
+		close(fds[1]);
+		splited = ft_split(argv[3], ' ');
 		if (!splited)
 			return (2);
 		if (execve(splited[0], splited, envp) < 0)
 			return (5);
 	}
+	close (fds[0]);
+	close (fds[1]);
+
+	waitpid(pid, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (0);
 }
