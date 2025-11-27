0078a52e905beba7f531166ccf9477dac2c2f6b8
joel <joelsantossouza08@gmail.com>
2025-11-27 21:52:16 +0000

    Could implement exec_pipe_chain, now i need to optimize the function and solve leakd

--- srcs/exec/exec_pipe_chain.c
+++ srcs/exec/exec_pipe_chain.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 15:10:00 by joesanto          #+#    #+#             */
-/*   Updated: 2025/11/27 20:59:31 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/11/27 21:43:01 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,27 @@
 int	exec_pipe_chain(size_t size, char **cmds, char **envp, int end[2])
 {
 	t_cmd	cmd;
-	int		pipe1[2];
-	int		pipe2[2];
+	int		fd[2];
 
-	if (pipe(pipe1) < 0)
+	if (pipe(fd) < 0)
 		return (-1);
 	cmd = (t_cmd){};
-	if (get_cmd(&cmd, *cmds, envp) < 0 || execve_pipe(&cmd, envp, end[0], pipe1[1]) < 0)
-		return (close_pipe(pipe1[0], pipe1[1]), free_cmd(&cmd), -1);
+	if (get_cmd(&cmd, *cmds++, envp) < 0 || execve_pipe(&cmd, envp, end[0], fd[1]) < 0)
+		return (close_pipe(fd[0], fd[1]), free_cmd(&cmd), -1);
+	size -= 2;
 	while (size-- && *cmds)
 	{
-		if (pipe(pipe2) < 0)
-			return (close_pipe(pipe1[0], pipe1[1]), -1);
-		close(pipe1[1]);
-		if (get_cmd(&cmd, *cmds, envp) < 0 || execve_pipe(&cmd, envp, pipe1[0], pipe2[1]) < 0)
-			return (close(pipe1[0]), free_cmd(&cmd), -1);
-		close(pipe1[0]);
-		if (pipe(pipe1) < 0)
-			return (close_pipe(pipe2[0], pipe2[1]), -1);
-		close(pipe2[1]);
-		if (get_cmd(&cmd, *cmds, envp) < 0 || execve_pipe(&cmd, envp, pipe2[0], pipe1[1]) < 0)
-			return (close(pipe2[0]), free_cmd(&cmd), -1);
-		close(pipe2[0]);
+		end[0] = fd[0];
+		close(fd[1]);
+		if (pipe(fd) < 0)
+			return (-1);
+		if (get_cmd(&cmd, *cmds++, envp) < 0 || execve_pipe(&cmd, envp, end[0], fd[1]) < 0)
+			return (close_pipe(fd[0], fd[1]), free_cmd(&cmd), -1);
+		close(end[0]);
 	}
-	close(pipe1[1]);
-	if (get_cmd(&cmd, *cmds, envp) < 0 || execve_pipe(&cmd, envp, pipe1[0], pipe2[1]) < 0)
-		return (close(pipe1[0]), free_cmd(&cmd), -1);
-	close(pipe1[0]);
+	close(fd[1]);
+	if (get_cmd(&cmd, *cmds, envp) < 0 || execve_pipe(&cmd, envp, fd[0], end[1]) < 0)
+		return (close(fd[0]), free_cmd(&cmd), -1);
+	close(fd[0]);
 	return (0);
 }
