f82d82b690d4a211398411e9718702f00b166ae8
joel <joelsantossouza08@gmail.com>
2025-11-28 10:17:55 +0000

    Testing with infile and outfile

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,31 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 12:23:39 by joesanto          #+#    #+#             */
-/*   Updated: 2025/11/27 21:04:34 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/11/28 10:17:32 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "pipex.h"
+#include <unistd.h>
+#include <fcntl.h>
 
 int	main(int argc, char **argv, char **envp)
 {
-	//if (argc < 5)
-	//{
-	//	ft_fprintf(2, "Usage: %s <file1> <cmd1> <cmd2> <file2>\n", *argv);
-	//	return (1);
-	//}
-	if (argc < 3)
+	int	file1;
+	int	file2;
+
+	if (argc < 5)
+	{
+		ft_fprintf(2, "Usage: %s <file1> <cmd1> <cmd2> [cmd...] <file2>\n", *argv);
 		return (1);
-	exec_pipe_chain(argc - 1, argv + 1, envp, (int [2]){0, 1});
-	return (0);
+	}
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 < 0)
+		return (2);
+	file2 = open(argv[argc - 1], O_WRONLY | O_CREAT, 0666);
+	if (file2 < 0)
+		return (close(file1), 3);
+	exec_pipe_chain(argc - 1, argv + 1, envp, (int [2]){file1, file2});
+	return (close(file1), close(file2), 0);
 }
