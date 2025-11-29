a95d61d6880722f0a6b4dfc2472c7756a65af2c8
joel <joelsantossouza08@gmail.com>
2025-11-29 21:32:59 +0000

    Truncate data of outfile, making it do not append

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 12:23:39 by joesanto          #+#    #+#             */
-/*   Updated: 2025/11/29 20:56:18 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/11/29 21:32:37 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ int	main(int argc, char **argv, char **envp)
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 < 0)
 		(write(STDERR_FILENO, "pipex: ", 7), perror(argv[1]));
-	file2 = open(argv[argc - 1], O_WRONLY | O_CREAT, 0666);
+	file2 = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (file2 < 0)
-		return (close(file1), 3);
+		return (close(file1), 2);
 	return (exec_pipe_chain(argc - 3, argv + 2, envp, (int [2]){file1, file2}));
 }
