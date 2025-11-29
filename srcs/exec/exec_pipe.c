527033b8942f995a5acd1c91889f6eb7643353b1
joel <joelsantossouza08@gmail.com>
2025-11-29 13:50:53 +0000

    Invert the order of print message error and free all data

--- srcs/exec/exec_pipe.c
+++ srcs/exec/exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 20:06:54 by joesanto          #+#    #+#             */
-/*   Updated: 2025/11/29 13:40:15 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/11/29 13:50:16 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,15 @@ int	exec_pipe(char *pcmd, char **envp, int pipe1[2], int pipe2[2])
 	if (pid != 0)
 		return (pid);
 	if (get_cmd(&cmd, pcmd, envp) < 0)
-		(close_pipe(pipe1), close_pipe(pipe2), perror("get_cmd"), exit(errno));
+		(perror("get_cmd"), close_pipe(pipe1), close_pipe(pipe2), exit(errno));
 	if (!cmd.path)
-		(clean_child(pipe1, pipe2, &cmd), cmd_not_found(cmd.argv[0]), exit(127));
+		(cmd_not_found(cmd.argv[0]), clean_child(pipe1, pipe2, &cmd), exit(127));
 	if (dup2(pipe1[0], STDIN_FILENO) < 0)
-		(clean_child(pipe1, pipe2, &cmd), perror("dup2"), exit(errno));
+		(perror("dup2"), clean_child(pipe1, pipe2, &cmd), exit(errno));
 	if (dup2(pipe2[1], STDOUT_FILENO) < 0)
-		(clean_child(pipe1, pipe2, &cmd), perror("dup2"), exit(errno));
+		(perror("dup2"), clean_child(pipe1, pipe2, &cmd), exit(errno));
 	close_pipe(pipe1);
 	close_pipe(pipe2);
 	execve(cmd.path, cmd.argv, envp);
-	(free_cmd(&cmd), perror("execve"), exit(errno));
+	(perror("execve"), free_cmd(&cmd), exit(errno));
 }
